48f0a83c64b8607016866996faf0a08fe047be2c
Imran Haque <ihaque@cs.stanford.edu>
2012-01-14 18:58:32 -0800

    Fixed bug in linearize_frames

--- linearize_frames.py
+++ linearize_frames.py
@@ -33,28 +33,32 @@ def jpegfiles(directory):
 def link_linear(sorted_files, tgtdir):
     ndigits = ceil(log10(len(sorted_files)))
     tgtdir = os.path.abspath(tgtdir)
-    out_names = []
+    out_names_and_times = []
     fileformat = "frame.%%0%dd.jpg" % ndigits
     if not os.path.exists(tgtdir):
         os.mkdir(tgtdir)
-    for idx, src in enumerate(sorted_files):
+    for idx, (time, src) in enumerate(sorted_files):
         base = os.path.split(src)[1]
         tgt = os.path.join(tgtdir, fileformat % idx)
-        out_names.append(tgt)
+        out_names_and_times.append((tgt, time))
         print "linking",base,"to",tgt
         os.symlink(src, tgt)
-    return out_names
+    return out_names_and_times
 
 def main(srcdir, tgtdir, exiflog):
     srcfiles = jpegfiles(srcdir)
     pool = Pool()
     digtimes = pool.map(getDigTime, srcfiles)
-    tagged_files =  filter(lambda xx:xx[0] is not None, zip(digtimes, srcfiles))
-    sorted_files = [fn for time, fn in sorted(tagged_files)]
-    out_names = link_linear(sorted_files, tgtdir)
+    sorted_files = sorted(filter(lambda xx:xx[0] is not None,
+                          zip(digtimes, srcfiles)))
+    out_names_and_times = link_linear(sorted_files, tgtdir)
     with open(exiflog,"w") as logf:
-        for time, name in izip(digtimes, out_names):
-            strtime = time.strftime("%Y:%m:%d %H:%M:%S")
+        for name, time in out_names_and_times:
+            try:
+                strtime = time.strftime("%Y:%m:%d %H:%M:%S")
+            except:
+                print time
+                print name
             logf.write("%s\t%s\n" % (name, strtime))
 
     return
